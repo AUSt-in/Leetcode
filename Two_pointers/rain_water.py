b80a40aff4c698cb3f97fae7cc453335e0038f6d
Austin Pereira <79310192+AUSt-in@users.noreply.github.com>
2024-05-26 20:39:48 -0700

    Update rain_water.py

--- Two_pointers/rain_water.py
+++ Two_pointers/rain_water.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        #using two pointers, main point to note is cube is one 
+        #using o(n*2) solution
+        l = 0
+        r= len(height)-1
+        l_max = height[l]
+        r_max = height[r]
+        maxWater = 0
+        while l < r:
+            if l_max < r_max:
+                l+=1
+                l_max = max(l_max,height[l])
+                maxWater+= l_max - height[l]
+            else:
+                r -=1
+                r_max = max(r_max,height[r])
+                maxWater += r_max - height[r]
+        return maxWater
